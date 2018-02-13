d44cd14c0f6d0562721ae5102a1554ecf6af9992
david <davidtnfsh@gmail.com>
2018-02-13 22:17:24 +0800

    update test.py for showing benchmark

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import sys, pyprind, subprocess, json, os
 
 modeList = ['tfidf', 'kcem', 'kcemCluster', 'hybrid', 'contextNetwork', 'pagerank']
 
-for topn in range(3, 10, 3):
+for topNum in range(3, 10, 3):
     for clusterTopn in range(3, 10, 3):
-        print("topn: {}, clusterTopn:{}".format(topn, clusterTopn))
+        print("topn: {}, clusterTopn:{}".format(topNum, clusterTopn))
         print('======================================')
         for mode in modeList:
-            b = Behavior2Text(mode, topn, clusterTopn)
+            b = Behavior2Text(mode, topNum, clusterTopn)
             b.buildTopn()
             topnsFile = json.load(open(b.output, 'r'))
             for refineData, fileName in topnsFile:
@@ -19,4 +19,6 @@ for topn in range(3, 10, 3):
                 else:
                     print(b.sentence(topn, fileName))
             print('NDCG is {}'.format(b.NDCG/len(topnsFile)))
+
+        for mode in modeList:
             os.remove(mode + '.json')
